a09bde92ac0289d33353d13eeb1b5af8d685950c
Brenton House <brenton.house@gmail.com>
2020-10-27 15:07:39 -0500

    update

--- index.js
+++ index.js
@@ -1,2 +1,1 @@
-// If you have different module names for iOS and Android, you will need an if-else
-module.exports = (Ti.Platform.osname === 'android') ? require('{{android_module_id}}') : require('{{ios_module_id}}');
+module.exports = {};
